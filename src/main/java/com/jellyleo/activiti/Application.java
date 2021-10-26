12ca7ed86cfe346f6c26d154fee9b10b08e70dd2
OfBen <jpzhou2017@126.com>
2021-10-26 17:23:29 +0800

    first init

---
+++ src/main/java/com/jellyleo/activiti/Application.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Jellyleo on 2019年12月16日
+ * Copyright © 2019 jellyleo.com 
+ * All rights reserved. 
+ */
+package com.jellyleo.activiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * 功能描述:应用启动类
+ *
+ * @author Jelly
+ * @created 2019年11月19日
+ * @version 1.0.0
+ */
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
